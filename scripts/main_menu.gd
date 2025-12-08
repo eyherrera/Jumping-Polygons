e7af4ef748b11e7d75cd93ac3b0982baa06d83a7
eyherrera <106132557+eyherrera@users.noreply.github.com>
2025-12-08 05:50:34 -0700

    create main level 2 structure

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 # Define your level paths here. 
 # Make sure these files exist, or the game will crash when clicked!
 const LEVEL_1_PATH = "res://scenes/levels/level_1.tscn"
-# const LEVEL_2_PATH = "res://scenes/levels/level_2.tscn" 
+const LEVEL_2_PATH = "res://scenes/levels/level_2.tscn" 
 # const LEVEL_3_PATH = "res://scenes/levels/level_3.tscn"
 
 func _ready():
@@ -18,7 +18,7 @@ func _on_level_1_pressed():
 
 func _on_level_2_pressed():
 	print("Level 2 not implemented yet!")
-	# get_tree().change_scene_to_file(LEVEL_2_PATH)
+	get_tree().change_scene_to_file(LEVEL_2_PATH)
 
 func _on_level_3_pressed():
 	print("Level 3 not implemented yet!")
