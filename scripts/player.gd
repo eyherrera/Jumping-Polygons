ee0874989035372f8e7d8d38741bda903e29a701
eyherrera <106132557+eyherrera@users.noreply.github.com>
2025-12-07 22:13:05 -0700

    added level 1 scene and tilemap to it

--- scripts/player.gd
+++ scripts/player.gd
@@ -26,7 +26,7 @@ func _physics_process(delta):
 
 	# 3. Handle Jump
 	# We use "ui_accept" (Space/Enter) by default
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
 
 	# 4. Move
