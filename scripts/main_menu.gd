77d53383c725c834d6963254533459c27d21b6df
eyherrera <106132557+eyherrera@users.noreply.github.com>
2025-12-09 02:00:42 -0700

    created new theme

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 # Define your level paths here. 
 # Make sure these files exist, or the game will crash when clicked!
-const LEVEL_1_PATH = "res://scenes/levels/level_1.tscn"
+const LEVEL_1_PATH = "res://scenes/levels/level_1.tscn" 
 const LEVEL_2_PATH = "res://scenes/levels/level_2.tscn" 
 const LEVEL_3_PATH = "res://scenes/levels/level_3.tscn"
 
@@ -17,11 +17,11 @@ func _on_level_1_pressed():
 	get_tree().change_scene_to_file(LEVEL_1_PATH)
 
 func _on_level_2_pressed():
-	#print("Level 2 not implemented yet!")
+	print("Level 2 not implemented yet!")
 	get_tree().change_scene_to_file(LEVEL_2_PATH)
 
 func _on_level_3_pressed():
-	#print("Level 3 not implemented yet!")
+	print("Level 3 not implemented yet!")
 	get_tree().change_scene_to_file(LEVEL_3_PATH)
 
 func _on_quit_pressed():
